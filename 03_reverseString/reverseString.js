8a1ecc5ec1190af059cc4afb63e77ecbd3d26fd9
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-12-10 08:35:16 +0100

    Insert body

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (text) {
+  const textArray = text.split("");
+  const reversedArray = textArray.reverse();
+  const reverseText = reversedArray.join("");
+  return reverseText;
 };
 
 // Do not edit below this line
