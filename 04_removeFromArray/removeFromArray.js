d39371e55f80e19feb8ece985b27cee026c27daa
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-12-10 09:36:12 +0100

    Write function body using array.filter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...rest) {
+  return array.filter((item) => !rest.includes(item));
 };
 
 // Do not edit below this line
