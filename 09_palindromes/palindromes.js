79806e9392dc70712fa060ee90f432bc90afb90e
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-12-13 14:48:52 +0100

    Fill functionbody to pass tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const array = string
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .split("");
 
+  const reverse = array.toReversed().join("");
+  const forward = array.join("");
+
+  return forward == reverse;
 };
 
 // Do not edit below this line
