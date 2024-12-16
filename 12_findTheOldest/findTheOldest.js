05a927b97b7a3600b2f2f8c818194f259cfb5483
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-12-16 09:48:02 +0100

    Write body to pass test
    First draft. Passes first test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  const sorted = arr.sort(function (a, b) {
+    const lastPerson = a.yearOfDeath - a.yearOfBirth;
+    const nextPerson = b.yearOfDeath - a.yearOfBirth;
+    return lastPerson > nextPerson ? -1 : 1;
+  });
+  return sorted[0];
 };
 
 // Do not edit below this line
