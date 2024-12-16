8dfa4df747b5bf0ed6d2101c0d563425b6b71331
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-12-16 08:30:08 +0100

    First iteration of answer

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let thisStep = 1;
+  let lastStep = 0;
 
+  for (let i = 1; i < num; i++) {
+    let temp = lastStep;
+    lastStep = thisStep;
+    thisStep += temp;
+  }
+  return thisStep;
 };
 
 // Do not edit below this line
