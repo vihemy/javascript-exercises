6ec68c906b8fa7e1fa3d1b5f18403f3f725c9fe6
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-12-16 10:03:04 +0100

    Add handling of missing yearOfDeath
    Inserts year of today in missing yearOfDeath

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,9 +1,15 @@
 const findTheOldest = function (arr) {
-  const sorted = arr.sort(function (a, b) {
-    const lastPerson = a.yearOfDeath - a.yearOfBirth;
-    const nextPerson = b.yearOfDeath - a.yearOfBirth;
-    return lastPerson > nextPerson ? -1 : 1;
-  });
+  const sorted = arr
+    .map(function (person) {
+      if (person.yearOfDeath === undefined)
+        person.yearOfDeath = new Date().getFullYear();
+      return person;
+    })
+    .sort(function (a, b) {
+      const lastPerson = a.yearOfDeath - a.yearOfBirth;
+      const nextPerson = b.yearOfDeath - b.yearOfBirth;
+      return lastPerson > nextPerson ? -1 : 1;
+    });
   return sorted[0];
 };
 
